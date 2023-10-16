5a8d32776911290fec6a41299a93d5a511c057c9
d3v-g <wang.jiaye.ti@gmail.com>
2023-10-16 16:33:31 +0100

    fix: supabase cookies dynamic server error when deploying

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation"
 export const dynamic = "force-dynamic"
 
 export default async function Home() {
+    cookies().getAll()
     const supabase = createServerComponentClient<Database>({ cookies })
 
     const {
